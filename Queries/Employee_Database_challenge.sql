e0b32e6ca837ae352692272f36f587d32c83720f
Salih Mohammed <76082772+Donik22@users.noreply.github.com>
2021-04-28 01:43:20 -0700

    adding to Resources folder

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -3,14 +3,6 @@ DROP TABLE unique_titles;
 DROP TABLE retiring_titles;
 DROP TABLE mentorship_eligibility;
 
--- Set up initial Table
-SELECT ti.emp_no , em.first_name, em.last_name,ti.title,ti.from_date,ti.to_date 
-INTO retirement_titles
-FROM employees em
-LEFT JOIN titles ti ON em.emp_no = ti.emp_no
-WHERE em.birth_date BETWEEN '1952-01-01' AND '1955-12-31';
-
-
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
@@ -21,7 +13,7 @@ FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
 
 -- Get Unique titles and count()
-SELECT COUNT(emp_no) , title
+SELECT COUNT(emp_no) AS emp_count , title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
@@ -39,3 +31,19 @@ ORDER BY e.emp_no ASC, ti.to_date DESC
 -- Check employee 10291 latest title
 SELECT * FROM titles 
 WHERE emp_no = 10291
+
+-- Total retiring titles 
+SELECT SUM(emp_count) FROM retiring_titles
+
+-- employees that are not retiring.
+SELECT d.dept_name, CAST(AVG(s.salary) AS DECIMAL(10,2)) AS avg_salary FROM salaries s
+LEFT JOIN dept_emp de ON  s.emp_no = de.emp_no
+LEFT JOIN departments d ON de.dept_no = d.dept_no
+LEFT JOIN unique_titles ut ON s.emp_no = ut.emp_no
+GROUP BY d.dept_name 
+ORDER BY avg_salary DESC
+
+
+
+ 
+
